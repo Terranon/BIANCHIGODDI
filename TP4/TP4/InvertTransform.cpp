6b084ef1a6b47f02e8576884ccbe07254ce4150c
unknown <maximilien.bianchi@gmail.com>
2019-03-20 10:57:24 -0400

    completion de InvertTransform

--- TP4/TP4/InvertTransform.cpp
+++ TP4/TP4/InvertTransform.cpp
@@ -12,5 +12,11 @@
 // Methode pour appliquer une transformation d'inversion à un Chunk
 void InvertTransform::transform(const Chunk_iterator& c, AbsAudioFile& outFile) const
 {
-	// A Completer...
+	Chunk_iterator tmp = c;
+	size_t j = 0;
+	for (size_t i = (c->size() - 1); i >= 0; i--) {
+		tmp.operator[](j) = (c.operator[](i));
+		j++;
+	}
+	outFile.addChunk(tmp);
 }
