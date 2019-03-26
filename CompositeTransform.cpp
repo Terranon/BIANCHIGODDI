691330fb4ac30aae58a81b7c674b748d0db325c4
ines goddi <inesgoddi@gmail.com>
2019-03-26 17:56:59 -0400

    dernieres modifications

---
+++ CompositeTransform.cpp
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////
+//  CompositeTransform.cpp
+//  Implementation of the Class CompositeTransform
+//  Created on:      13-mar.-2017
+//  Original author: francois
+///////////////////////////////////////////////////////////
+
+#include "CompositeTransform.h"
+#include "AbsAudioFile.h"
+#include "ChunkIterator.h"
+
+CompositeTransform::CompositeTransform(const CompositeTransform & mdd)
+{
+   // A Completer...
+	for (auto it = mdd.begin;it != mdd.end; it++) {
+		addChild(*it);
+	}
+   
+}
+
+// Cloner la transformation composite et ses commandes enfant
+CompositeTransform * CompositeTransform::clone(void) const
+{
+    // A Completer...
+	CompositeTransform *newObject = new CompositeTransform (*this);
+	return (newObject);
+}
+
+// Executer les transformations enfant
+void CompositeTransform::transform(const Chunk_iterator& c, AbsAudioFile& outFile) const
+{ 
+	auto * it =  m_transforms.begin();
+	for (it; it != m_transforms.end();it ++){
+	  it
+	}
+	
+}
+
+void CompositeTransform::addChild(const AbsTransform& t)
+{ 
+	// A Completer...
+	m_transforms.push_back(t.clone);
+	
+}
+
+void CompositeTransform::removeChild(TransformIterator_const transfIt)
+{
+	// A Completer...
+	m_transforms.erase(transfIt);
+}
